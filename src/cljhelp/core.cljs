783f6c31e0f1005695e982cd630893768688f1d0
Greg Allen <Greg Allen>
2017-01-28 18:41:06 -0600

    Add conditional redirect to another page

--- src/cljhelp/core.cljs
+++ src/cljhelp/core.cljs
@@ -2,15 +2,30 @@
   (:require [clojure.browser.dom :as dom]
  ))
 
-(enable-console-print!)
-
 (defn paste-value
  " help from https://gist.github.com/kohyama/6192253"
   [t]
-  (let [el (dom/get-element "text")
-        highlighted-value (aget t "data")
+  (let [spot (dom/get-element "spot")
+        ;el (dom/get-element "text")
+        el (dom/element "textarea" {:cols 50 :rows 10})
+        highlighted-value t
+        set-the-valude (dom/set-text el highlighted-value)
        ]
-    (dom/set-text el highlighted-value)))
+        (dom/append spot el)
+    ))
+
+(defn redirect-to-cheatsheet [url] 
+  (let [spot (dom/get-element "spot")
+        ifr (dom/element "iframe" {:width "100%" :height "100%" :frameborder "0"})
+        set-the-url (set! (.-src ifr) url)
+       ]
+    (dom/append spot ifr)
+  ))
+
+(defn handle-highlight [t]
+  (let [txt (aget t "data")]
+  (if (= txt "page") (redirect-to-cheatsheet "https://clojure.org/api/cheatsheet")
+    (paste-value txt))))
   
 (defn current-tab
   "taken from https://github.com/marcelocra/chrome-extension-cljs-example/blob/master/src/chrome_extensions/background/events.cljs"
@@ -21,7 +36,7 @@
             (cb (first tabs)))))
 
 (defn pasteSelection []
-  (let [f1 (fn [tab] (.sendMessage js/chrome.tabs (aget tab "id") #js {:method "getSelection"} paste-value))
+  (let [f1 (fn [tab] (.sendMessage js/chrome.tabs (aget tab "id") #js {:method "getSelection"} handle-highlight))
        ]
   (current-tab f1)))
 
